6fb7cd1cc6365a3c6c1fabd8c8e31e5af0482146
youjiayi <youjiayi@localhost>
2021-07-26 16:21:29 +0800

    Java

--- study/src/main/java/com/study/day05/HelloBMI.java
+++ study/src/main/java/com/study/day05/HelloBMI.java
@@ -4,20 +4,11 @@ public class HelloBMI {
 
 	public static void main(String[] args) {
 		BMI b1 = new BMI("John", 170, 60);
-<<<<<<< HEAD
 		// 查看目前的屬性資料
 //		System.out.println(b1.getName());
 //		System.out.println(b1.getHeight());
 //		System.out.println(b1.getWeight());
 //		System.out.println(b1.getBmi());
-=======
-		//查看目前屬性資料
-		System.out.println(b1.getName());
-		System.out.println(b1.getHeight());
-		System.out.println(b1.getWeight());
-		System.out.println(b1.getBmi());
-		
->>>>>>> branch 'master' of https://github.com/you-jia-yi/java20210726.git
 		System.out.println(b1);
 		
 		// 變更身高
