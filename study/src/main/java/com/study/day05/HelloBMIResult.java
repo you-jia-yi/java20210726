27152f4215f7b84edd9790e0a002ea1090716307
youjiayi <youjiayi@localhost>
2021-07-26 15:18:35 +0800

    java

--- study/src/main/java/com/study/day05/HelloBMIResult.java
+++ study/src/main/java/com/study/day05/HelloBMIResult.java
@@ -4,7 +4,7 @@ public class HelloBMIResult {
 
 	public static void main(String[] args) {
 		// John, 170, 60, 是否標準？
-		BMI bmi = new BMI(John, 170, 60);
+		BMI bmi = new BMI("John", 170, 60);
 		BMIResult bmiResult = new BMIResult(bmi);
 		System.out.println(bmiResult.getResult());
 
