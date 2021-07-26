f79846f841063f4255762af9741d93e60e76e7b2
youjiayi <youjiayi@localhost>
2021-07-26 16:18:09 +0800

    Java

--- study/src/main/java/com/study/day05/BMIResult.java
+++ study/src/main/java/com/study/day05/BMIResult.java
@@ -1,19 +1,20 @@
 package com.study.day05;
 
+// 取得 BMI 結果 : 過重, 過輕, 標準
+// 設計模式: 組合
 public class BMIResult {
 	private String result;
 	private BMI bmi;
-
+	
 	public BMIResult(BMI bmi) {
-		super();
 		this.bmi = bmi;
-		//分析
+		// 分析
 		double bmiValue = this.bmi.getBmi();
-		if(bmiValue>23) {
+		if(bmiValue > 23) {
 			result = "過胖";
-		}else if (bmiValue<=18) {
+		} else if (bmiValue <= 18) {
 			result = "過輕";
-		}else {
+		} else {
 			result = "正常";
 		}
 	}
@@ -22,7 +23,10 @@ public class BMIResult {
 		return result;
 	}
 
-	
+	@Override
+	public String toString() {
+		return "BMIResult [result=" + result + ", bmi=" + bmi + "]";
+	}
 	
 	
 }
--- study/src/main/java/com/study/day05/HelloBMI.java
+++ study/src/main/java/com/study/day05/HelloBMI.java
@@ -4,20 +4,17 @@ public class HelloBMI {
 
 	public static void main(String[] args) {
 		BMI b1 = new BMI("John", 170, 60);
-		//查看目前屬性資料
-		System.out.println(b1.getName());
-		System.out.println(b1.getHeight());
-		System.out.println(b1.getWeight());
-		System.out.println(b1.getBmi());
-		
-		System.out.println(b1);
-		//System.out.println(b1.toString());
-		
-		//變更身高
-		b1.setHeight(188);
-		//查看屬性資料
+		// 查看目前的屬性資料
+//		System.out.println(b1.getName());
+//		System.out.println(b1.getHeight());
+//		System.out.println(b1.getWeight());
+//		System.out.println(b1.getBmi());
 		System.out.println(b1);
 		
+		// 變更身高
+		b1.setHeight(180);
+		// 查看目前的屬性資料
+		System.out.println(b1);
 		
 	}
 
