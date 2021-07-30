803cf45819ba6bdee0da8d9d734d7b64bfb6123e
youjiayi <youjiayi@localhost>
2021-07-30 13:39:13 +0800

    Java

---
+++ study/src/main/java/com/study/day12/WrapperDemo5.java
@@ -0,0 +1,29 @@
+package com.study.day12;
+
+import java.util.Arrays;
+
+public class WrapperDemo5 {
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee("John", 20, 40000);
+		Employee e2 = new Employee("Mary", 30, 50000);
+		Employee e3 = new Employee("Helen", 25, null);
+		Employee e4 = new Employee("Jim", 40, 60000);
+		Employee e5 = new Employee("Jack", 35, 55000);
+		
+		// 試問公司平均年齡 = ?
+		Employee[] employees = {e1, e2, e3, e4, e5};
+		
+		double avgOfAge = Arrays.stream(employees)
+			//.peek(System.out::println)
+			.mapToInt(Employee::getAge)
+			//.peek(System.out::println)
+			.average()
+			.getAsDouble();
+		System.out.println(avgOfAge);
+		
+		// 試問公司平均薪資 = ?
+		
+	}
+
+}
